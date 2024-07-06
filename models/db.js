ee6b2402cdadfc7c84045c4d0bba3a535877a707
Tathagata17 <tathagata14n14@gmail.com>
2024-07-06 15:26:14 +0530

    another route added 1.0.0

--- models/db.js
+++ models/db.js
@@ -1,14 +1,15 @@
-const mongoose=require("mongoose");
+const mongoose = require("mongoose");
 
-const connectdb =async()=>{
+const connectdb = async () => {
     try {
-       const resposne = await mongoose.connect(process.env.mongoUrl)
-       console.log("db connected");
+        await mongoose.connect(process.env.MONGODB_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log("db connected");
     } catch (error) {
-        console.log("mongo error is:",error);
+        console.log("mongo error is:", error);
     }
-}
+};
 
-module.exports=connectdb;
-
- 
+module.exports = connectdb;
